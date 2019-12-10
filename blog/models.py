5011eb2c5014ba079b1012d50baefc6c240fd471
a <a.b@c.d>
2019-12-10 23:21:01 +0200

    commit

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE,related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Categories(models.Model):
+    parent = models.ForeignKey('self', models.SET_NULL, blank=True, null=True,
+                                related_name='parent_category')
+    name = models.CharField(max_length=30)
+    enabled = models.BooleanField()
